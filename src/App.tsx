cef7e30c025d0558529c6effd9253d62ab474144
Olav Liljeberg <sofnbeat@gmail.com>
2025-09-10 17:42:27 +0200

    Tester ut index.css. fjernet mye av innhold.  Lagt inn ipsum lorem

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,27 @@ function App() {
       <header>
           <h1>Test side</h1>
       </header>
-        <header>
+
             <main>
                 <article>
                     <h2>Tittel på artikkel</h2>
+                    <h3>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non accumsan lacus. Maecenas a elementum neque. Integer iaculis congue erat in bibendum. Integer ac mi euismod, convallis est id, convallis ante. Suspendisse laoreet tortor convallis tortor pellentesque, lacinia molestie ligula rhoncus. Vestibulum luctus tincidunt turpis nec convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec sagittis sit amet magna vel posuere. Praesent ac porta tortor. Sed libero dui, iaculis et tellus tempor, hendrerit blandit diam. Fusce iaculis quam justo, eu efficitur mi mollis quis. Donec suscipit felis ante, elementum vulputate nulla malesuada ut. Nulla varius porta turpis eu semper. Nam in nunc vel nulla luctus ultricies. Ut at fringilla nunc. Ut sed congue nisi.
+
+                        Phasellus finibus, diam vel volutpat tempus, justo sem iaculis eros, non sodales mauris enim id arcu. Praesent aliquet turpis et dui venenatis aliquet. Morbi ultrices elit eget lectus vulputate ornare. Vestibulum id sem tempor, interdum massa et, efficitur massa. Curabitur maximus risus nec urna pretium, quis vehicula nulla varius. Phasellus nec tempus dui. Aliquam eget nibh mattis, viverra turpis ut, aliquam neque. Duis suscipit arcu ligula, id rutrum ex euismod eu. Donec ultrices erat sit amet felis tristique mattis. Praesent finibus finibus eros. Maecenas at ipsum congue, condimentum magna eget, sagittis mi. Maecenas interdum feugiat ultrices. Maecenas congue pellentesque scelerisque.
+
+                        Cras dignissim pulvinar nulla, id consectetur dolor egestas a. Nullam ac enim nec lectus rutrum fermentum vitae vitae risus. Nunc porttitor sit amet justo in cursus. Duis sed massa diam. Nunc tellus metus, euismod nec eros eget, dignissim convallis nulla. Pellentesque rhoncus sapien id porta sodales. Duis scelerisque, libero vel interdum condimentum, nisi odio accumsan justo, a aliquet ex dui at sapien. Quisque rutrum nulla vitae ligula lacinia fermentum. Vestibulum id venenatis elit. In at est malesuada, iaculis est nec, tristique purus.
+
+                        Quisque eget gravida metus, eu consectetur neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer fermentum leo vehicula sapien egestas molestie. Praesent eros ante, gravida ut arcu sed, rhoncus pharetra mi. Mauris eu orci facilisis, feugiat odio ut, mattis lectus. Proin sit amet maximus mi, sed congue purus. Praesent laoreet massa in libero dignissim scelerisque. Cras turpis ex, efficitur eget porttitor in, varius eget odio. Nam molestie, turpis a egestas tempor, nisl ipsum finibus elit, ut suscipit urna lacus a arcu. Duis magna ligula, hendrerit congue tincidunt ac, iaculis interdum nibh. Ut vitae nunc ut felis pulvinar consectetur. Cras eget efficitur tellus. Praesent eleifend lobortis augue ac interdum.
+
+                        Aliquam risus metus, luctus ac pellentesque a, egestas eu nisi. Suspendisse potenti. Morbi augue nunc, gravida eu ipsum eu, interdum ornare elit. Curabitur quis ipsum leo. Ut maximus viverra scelerisque. Donec eu tincidunt eros. Nulla facilisi. Fusce eu elementum felis, et vestibulum mi. Fusce sit amet cursus risus, vel scelerisque odio. In sit amet lobortis augue, id luctus enim. Curabitur rutrum dui eget sem ultricies ornare. Vestibulum id ex in turpis suscipit viverra nec at nisl.
+                    </p>
+
                 </article>
+
+
             </main>
-        </header>
     </>
   )
 }
